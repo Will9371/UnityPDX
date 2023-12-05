22fabdb123faabf02f9a66d2853711fd7b367cd5
Will Petillo <will.petillo@gmail.com>
2023-12-04 20:07:53 -0800

    Dynamic mass of asteroids

--- Assets/Asteroid/Asteroid.cs
+++ Assets/Asteroid/Asteroid.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] Rigidbody rb;
+    
+    [Header("Settings")]
     public float startPositionRange;
     public float speedRange;
     public float scaleMaximum;
     public float scaleMinimum;
+    
+    [Header("Calculated/Debug")]
+    public float volume;
 
     void Start()
     {
@@ -17,6 +23,11 @@ public class Asteroid : MonoBehaviour
     {
         transform.position = Random.insideUnitSphere * startPositionRange;
         transform.localScale = Random.Range(scaleMinimum, scaleMaximum) * Vector3.one;
+        
+        var scaleX = transform.localScale.x;
+        var radius = scaleX/2f;
+        volume = 4f/3f * Mathf.PI * Mathf.Pow(radius, 3);
+        rb.mass = volume;
         rb.velocity = Random.insideUnitSphere * speedRange;
     }
 }
